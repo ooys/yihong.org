625d0b0fa91a685c8689e8ccf957e1d647c793cc
Yihong(Michael) Song <46873721+michaelsong4399@users.noreply.github.com>
2022-01-05 11:07:36 -0500

    Init Project

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
 import "../styles/globals.css";
+import Router from "next/router";
+import NProgress from "nprogress"; //nprogress module
+import "../node_modules/nprogress/nprogress.css"; //styles of nprogress
+import React, { useEffect } from "react";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
-import Head from "next/head";
-import Image from "next/image";
-
 export default function Home() {
-    return <div>Hello World.</div>;
+    return (
+        <div className="absolute w-screen">
+            <div className="static bg-dark h-screen text-center pt-[20vh]">
+                <div className="text-pink font-title text-[14vw] md:text-[10vw] lg:text-[8vw] ">
+                    Hello.
+                </div>
+                <div className="text-white font-title text-[4vw] md:text-[3vw] lg:text-[2.5vw] ">
+                    I am Yihong Song.
+                </div>
+            </div>
+        </div>
+    );
 }
