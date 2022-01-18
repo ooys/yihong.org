06d2962c3e74c69d24c96c46690fb6213770879f
Yihong(Michael) Song <46873721+michaelsong4399@users.noreply.github.com>
2022-01-18 03:21:32 -0500

    Update Front

---
+++ components/animations/Home.js
@@ -0,0 +1,16 @@
+function HomeAnimation() {
+    return (
+        <div className="absolute w-screen">
+            <div className="static bg-dark h-screen text-center pt-[20vh]">
+                <div className="text-pink font-title text-[14vw] md:text-[10vw] lg:text-[8vw] ">
+                    Hello.
+                </div>
+                <div className="text-white font-title text-[4vw] md:text-[3vw] lg:text-[2.5vw] ">
+                    I am Yihong Song.
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HomeAnimation;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 export default function Home() {
     return (
-        <div className="absolute w-screen">
-            <div className="static bg-dark h-screen text-center pt-[20vh]">
-                <div className="text-pink font-title text-[14vw] md:text-[10vw] lg:text-[8vw] ">
-                    Hello.
+        <div className="absolute w-[100%]">
+            <div className="static bg-dark h-screen pt-[20vh] px-[15vw] grid grid-cols-1 lg:grid-cols-2 lg:gap-4">
+                <div className="text-left ">
+                    <div className="text-white font-title text-[4vw] md:text-[2vw] lg:text-[1.5vw] ">
+                        I am
+                    </div>
+                    <div className="text-white font-title text-[8vw] md:text-[4vw] lg:text-[4vw] ">
+                        Yihong Song.
+                    </div>
+                    <div className="text-white font-title text-[3vw] md:text-[2vw] lg:text-[1vw] ">
+                        Developer, Composer, Entrepreneur
+                    </div>
                 </div>
-                <div className="text-white font-title text-[4vw] md:text-[3vw] lg:text-[2.5vw] ">
-                    I am Yihong Song.
-                </div>
+                <div className=""></div>
             </div>
         </div>
     );
