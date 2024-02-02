a7944c5d825600f7f17d5067b26b5e2c33f77bb2
Yihong Song <ys377@duke.edu>
2024-02-02 03:46:00 -0500

    clear v2

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,5 @@
-import "../styles/globals.css";
-import Router from "next/router";
-import NProgress from "nprogress"; //nprogress module
-import "../node_modules/nprogress/nprogress.css"; //styles of nprogress
-import React, { useEffect } from "react";
-import Head from "next/head";
+import "@/styles/globals.css";
 
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
-
-function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                <title>Yihong Song</title>
-            </Head>
-            <Component {...pageProps} />
-        </>
-    );
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
 }
-
-export default MyApp;
